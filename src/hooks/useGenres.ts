296b2b8bf4d4c4987aa378b83a7fcf0313ed61b8
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-06-24 19:53:14 +0600

    genre display

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-import { Game } from "./useGames";
 import useData from "./useData";
 
 export interface Genre {
