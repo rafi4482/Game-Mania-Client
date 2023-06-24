3565881de504b11d23643d2fc064c88bd05028af
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-06-24 19:43:42 +0600

    genre display

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,10 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
     >
       <GridItem area="nav">
         <NavBar  />
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useData from '../hooks/useData';
 import useGenres, { Genre } from '../hooks/useGenres'
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
 
-    const {data} = useData<Genre>('/genres');
+   const { data, isLoading, error } = useGenres();
 
   return (
-    <ul>
-        {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
+    <List>
+        {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+       <Image
+       boxSize='32px'
+       borderRadius={8}
+      src={getCroppedImageUrl(genre.image_background)}
+       />
+       <Text fontSize='lg'>{genre.name}</Text>
+              </HStack>
+        </ListItem>)}
         
-    </ul>
+    </List>
   )
 }
 
