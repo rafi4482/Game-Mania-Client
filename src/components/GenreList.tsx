1d25749bc272769f67151c0b6c4c0228975619d6
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-06-24 19:52:54 +0600

    genre display

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useData from '../hooks/useData';
 import useGenres, { Genre } from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url';
@@ -7,10 +7,20 @@ const GenreList = () => {
 
    const { data, isLoading, error } = useGenres();
 
+if(error) return null;
+
+   if (isLoading) return <Spinner
+  thickness='4px'
+  speed='0.65s'
+  emptyColor='gray.200'
+  color='green.500'
+  size='xl'
+/>
+
   return (
     <List>
         {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
+          <HStack cursor="pointer">
        <Image
        boxSize='32px'
        borderRadius={8}
